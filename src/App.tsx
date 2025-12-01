3485583fbcf2cd2e27e0ce2ce7de6bfa03e7e346
Fernando Barrera <barrerafernando54@gmail.com>
2025-11-30 22:29:49 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className="min-h-screen bg-white">
+    <div className="min-h-screen bg-white overflow-x-hidden">
       <Navbar />
       <Hero />
       <About />
